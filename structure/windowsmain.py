492108992df04db4022259fbff4f6a84dbf01594
scwall <deselys.pascal@gmail.com>
2017-08-21 23:41:55 +0200

    Creating images of the ground, walls, and bosses

--- structure/windowsmain.py
+++ structure/windowsmain.py
@@ -1,20 +1,23 @@
 from pygame import *
 import pygame, os, sys, random
 from structure.loadmap import LoadMap
-from structure.variables import *
 # Initialise Class for Windows main
-class WindowsMain(LoadMap):
+class WindowsMain(LoadMap,pygame.sprite.Sprite):
 
-    def __init__(self, width=640, height=480):
+    def __init__(self, width=640, height=640):
+        pygame.sprite.Sprite.__init__(self)
         pygame.init()
         LoadMap.__init__(self)
         self.width = width
         self.height = height
-        self.image = "pictures/background.png"
-        self.background = pygame.image.load(self.image)
+        self.wallpicture = "pictures/wall.png"
+        self.floorpicture = "pictures/floor.png"
+        self.bosspicture = "pictures/boss.png"
+        self.boss = pygame.image.load(self.bosspicture)
+        self.wall = pygame.image.load(self.wallpicture)
+        self.floor = pygame.image.load(self.floorpicture)
         self.screen = pygame.display.set_mode((self.width, self.height))
-        self.display = pygame.display.set_caption(title_main)
-        self.flip = pygame.display.flip()
+        self.display = pygame.display.set_caption("Help MacGyver to escape")
         self.readFolderMap()
         self.createMapList()
         self.ScreenSprite()
@@ -27,18 +30,23 @@ class WindowsMain(LoadMap):
             print(len(self.mapList))
             while ii != len(self.mapList[i]):
                 if self.mapList[i][ii] == "W":
-                    self.screen.blit(self.background, ((i * 20),(ii * 20)))
+                    self.screen.blit(self.wall, ((i * 40),(ii * 40)))
+                if self.mapList[i][ii] == "F":
+                    self.screen.blit(self.floor, ((i * 40),(ii * 40)))
+                if self.mapList[i][ii] == "B":
+                    self.screen.blit(self.boss, ((i * 40), (ii * 40)))
                 ii += 1
             ii = 0
             i += 1
 
     def mainloop(self):
-        continuer = True
+        continues = True
 
-        # Boucle infinie
-        while continuer:
+        while continues:
             for event in pygame.event.get():
                 if event.type == QUIT:
-                    continuer = False
-                if event.type == KEYDOWN and event.key == K_SPACE:
-                    print("Espace")
+                    continues = False
+                if event.type == KEYDOWN and event.key == K_DOWN:
+                    pass
+
+
