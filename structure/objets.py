8c00ecb5d4133c754e1293ef398e7a11219f64d4
scwal <deselys.pascal@gmail.com>
2017-08-28 16:07:13 +0200

    add class objet

--- structure/objets.py
+++ structure/objets.py
@@ -1,1 +1,20 @@
+#Trouver image pour la seringle
+import os
+
+import pygame
+
+
+class Objet():
+    def __init__(self, *picture):
+        self.list = []
+        for picture_tuple in picture:
+            self.image = pygame.image.load(self.roadPictures(str(picture_tuple))).convert()
+            self.rect = self.image.get_rect()
+            self.list.append(self.rect)
+        print(self.list)
+        print(self.image)
+
+    def roadPictures(self, fichier):
+        return os.path.join('pictures', fichier)
+
 
