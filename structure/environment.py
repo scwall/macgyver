b6c356f2771e8501c936ae67e7158439589ac505
scwal <deselys.pascal@gmail.com>
2017-08-28 16:11:39 +0200

    add class objet, add picture modify class environment

--- structure/environment.py
+++ structure/environment.py
@@ -3,9 +3,8 @@ import os
 import pygame
 
 
-class Environment(pygame.sprite.Sprite):
+class Environment():
     def __init__(self, picture):
-        super().__init__()
         self.image = pygame.image.load(self.roadPictures(str(picture))).convert()
         self.rect = self.image.get_rect()
 
